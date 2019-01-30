18f0816e32de2d3b4caebb10c397b10aacb56ef6
KarthikBoddu <karthikb5566@gmail.com>
2019-01-30 19:32:22 +0530

    30jan

--- ObjectOrientedPrograms/Regex.php
+++ ObjectOrientedPrograms/Regex.php
@@ -1,12 +1,27 @@
 <?php
 include ('utility.php');
-$str = " Hello <<name>>, We have your full
-name as <<full name>> in our system. your contact number is 91­xxxxxxxxxx.
-Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.";
+$str = ' Hello <<name>>, We have your full
+name as <<full name>> in our system. your contact number is 91-­xxxxxxxxxx.
+Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.';
 
-$regex = "{10}x";
-$new_string = preg_replace($regex, "91-­xxxxxxxxxx", "903");
-echo $new_string;
+$regex = array("/<{2}\w+>{2}/ ","/<{2}\w+\s\w+>{2}/ ","/x{10}/ "," /\d+.\d.\d+/");
+$pattern = '${2}';
+   
+echo "enter the name \n";
+$name = Oops::readString();
+$str = preg_replace($regex[0], $name, $str);
+echo "enter full name \n";
+$fName = Oops::readString();
+$str = preg_replace($regex[1],$fName,$str);
+echo "enter phone number \n";
+$phNo = Oops::readInt();
+$str = preg_replace($regex[2],$phNo,$str);   
+echo "enter the date \n";
+$date = date();
+$str = preg_replace($regex[3],$date,$str);
+
+echo $str; 
+
 
 
 
@@ -18,5 +33,28 @@ echo $new_string;
 // $new = preg_replace("<{2}\\w.>{2}","vg",$str);
 
 // print $new;
+
+// [
+//     {
+        
+//             "name": "Biryani rice",
+//             "weight": "5",
+//             "price": "80"
+        
+//     },
+//     {
+        
+//             "name": "pulseType",
+//             "weight": "5",
+//             "price": "30"
+        
+//     },
+//     {
+	
+// 		"name":"wheats type ",
+// 		"weight":"6",
+// 		"price":"50"
+//     }	
+// ]
 
 ?>
--- ObjectOrientedPrograms/readJson.php
+++ ObjectOrientedPrograms/readJson.php
@@ -1,28 +1,49 @@
 <?php
-$str = '[
-    {
-        
-            "name": "Biryani rice",
-            "weight": "5",
-            "price": "80"
-        
-    },
-    {
-        
-            "name": "pulseType",
-            "weight": "5",
-            "price": "30"
-        
-    },
-    {
-	
-		"name":"wheats type ",
-		"weight":"6",
-		"price":"50"
-    }	
-]';
+class Inventory{
+    function readJson($file){
+        $fileStr = file_get_contents($file);
+        $jsonData = json_decode($fileStr,true);
+      //  print_r($jsonData);
+        return $jsonData;
+    }
 
-$json = json_decode($str);
-print_r($json);
+    function print($jsonData){
+        $str = array('Rice','Wheat','Pulses');
+        for($i=0;$i<sizeof($str);$i++){
+            $inven = $jsonData["$str[$i]"];
+
+            foreach($inven as $groc){
+                $name = $groc['name'];
+                $weight = $groc['weight'];
+                $price = $groc['price'];
+                printf('%s  value %d  '.PHP_EOL, $name, $weight*$price);
+            }
+        }
+    }
+}
+
+
+$file = "sample.json";
+$inventory = new Inventory();
+$jData = $inventory->readJson($file);
+$inventory->print($jData);
+
+
+
+
+// $rice = $data['Rice'];
+
+// foreach ($rice as $r) {
+//     $name = $r['name'];
+//     $weight = $r['weight'];
+//     $price = $r['price'];
+//     printf('%s weight value %d  '.PHP_EOL, $name, $weight*$price);
+   
+// }
+
+
+// print_r($data);
+
 
 ?>
+
